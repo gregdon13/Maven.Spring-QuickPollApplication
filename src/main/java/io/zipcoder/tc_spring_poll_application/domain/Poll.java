3165a476787b739ca6f30767f393f8a87ec707f5
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-04-19 22:42:53 -0400

    Option and Poll made

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Poll.java
@@ -0,0 +1,35 @@
+package io.zipcoder.tc_spring_poll_application.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Poll {
+    private @Id @GeneratedValue @Column(name = "POLL_ID") Long id;
+    private @Column(name = "QUESTION") String question;
+    private @OneToMany(cascade = CascadeType.ALL) @JoinColumn(name = "POLL_ID") @OrderBy Set<Option> optionSet;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Set<Option> getOptionSet() {
+        return optionSet;
+    }
+
+    public void setOptionSet(Set<Option> optionSet) {
+        this.optionSet = optionSet;
+    }
+}
