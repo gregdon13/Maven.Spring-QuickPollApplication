716c842be3ba8e4356958f54ac28750855f707b2
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-04-19 23:01:51 -0400

    Vote class made

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Vote.java
@@ -0,0 +1,25 @@
+package io.zipcoder.tc_spring_poll_application.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Vote {
+    private @Id @GeneratedValue @Column(name = "VOTE_ID") Long id;
+    private @ManyToOne @JoinColumn(name = "OPTION_ID") Option option;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Option getOption() {
+        return option;
+    }
+
+    public void setOption(Option option) {
+        this.option = option;
+    }
+}
