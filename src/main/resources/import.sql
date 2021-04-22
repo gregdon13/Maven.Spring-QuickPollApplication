28f88583993fd88a5cfb6734f40b8e30196ce8bb
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-04-22 15:28:47 -0400

    LET'S GO! Got pagination to work!

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,97 +8,97 @@ insert into option (option_id, option_value, poll_id) values (3, 'Green', 1);
 -- 2 Favorite transportaion
 insert into poll (poll_id, question) values (2, 'What is your favorite mode of transportation?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'Car', 2);
-insert into option (option_id, option_value, poll_id) values (2, 'Boat', 2);
-insert into option (option_id, option_value, poll_id) values (3, 'Plane', 2);
+insert into option (option_value, poll_id) values ('Car', 2);
+insert into option (option_value, poll_id) values ('Boat', 2);
+insert into option (option_value, poll_id) values ('Plane', 2);
 
 -- 3 Favorite Sport
 insert into poll (poll_id, question) values (3, 'What is your favorite sport?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'Basketball', 3);
-insert into option (option_id, option_value, poll_id) values (2, 'Football', 3);
-insert into option (option_id, option_value, poll_id) values (3, 'Hockey', 3);
+insert into option (option_value, poll_id) values ('Basketball', 3);
+insert into option (option_value, poll_id) values ('Football', 3);
+insert into option (option_value, poll_id) values ('Hockey', 3);
 
 -- 4 Favorite Genre
 insert into poll (poll_id, question) values (4, 'What is your favorite genre of music?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'EDM', 4);
-insert into option (option_id, option_value, poll_id) values (2, 'Rock', 4);
-insert into option (option_id, option_value, poll_id) values (3, 'Hip Hop', 4);
+insert into option (option_value, poll_id) values ('EDM', 4);
+insert into option (option_value, poll_id) values ('Rock', 4);
+insert into option (option_value, poll_id) values ('Hip Hop', 4);
 
 -- 5 Favorite Comic book universe
 insert into poll (poll_id, question) values (5, 'What is your favorite comic book universe?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'DC', 5);
-insert into option (option_id, option_value, poll_id) values (2, 'Marvel', 5);
-insert into option (option_id, option_value, poll_id) values (3, 'Dark Horse', 5);
+insert into option (option_value, poll_id) values ('DC', 5);
+insert into option (option_value, poll_id) values ('Marvel', 5);
+insert into option (option_value, poll_id) values ('Dark Horse', 5);
 
 -- 6 Favorite Marvel
 insert into poll (poll_id, question) values (6, 'Who is your favorite Marvel hero?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'Thor', 6);
-insert into option (option_id, option_value, poll_id) values (2, 'Iron Man', 6);
-insert into option (option_id, option_value, poll_id) values (3, 'Captain Marvel', 6);
+insert into option (option_value, poll_id) values ('Thor', 6);
+insert into option (option_value, poll_id) values ('Iron Man', 6);
+insert into option (option_value, poll_id) values ('Captain Marvel', 6);
 
 -- 7 Favorite DC
 insert into poll (poll_id, question) values (7, 'Who is your favorite DC hero?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'Batman', 7);
-insert into option (option_id, option_value, poll_id) values (2, 'Wonder Woman', 7);
-insert into option (option_id, option_value, poll_id) values (3, 'Superman', 7);
+insert into option (option_value, poll_id) values ('Batman', 7);
+insert into option (option_value, poll_id) values ('Wonder Woman', 7);
+insert into option (option_value, poll_id) values ('Superman', 7);
 
 -- 8 Favorite DC villain
 insert into poll (poll_id, question) values (8, 'Who is your favorite DC villain?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'Lex Luthor', 8);
-insert into option (option_id, option_value, poll_id) values (2, 'Mr. Freeze', 8);
-insert into option (option_id, option_value, poll_id) values (3, 'Darseid', 8);
+insert into option (option_value, poll_id) values ('Lex Luthor', 8);
+insert into option (option_value, poll_id) values ('Mr. Freeze', 8);
+insert into option (option_value, poll_id) values ('Darseid', 8);
 
 -- 9 Favorite Marvel villain
 insert into poll (poll_id, question) values (9, 'Who is your favorite Marvel villain?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'Red Skull', 9);
-insert into option (option_id, option_value, poll_id) values (2, 'Ego', 9);
-insert into option (option_id, option_value, poll_id) values (3, 'Dormamu', 9);
+insert into option (option_value, poll_id) values ('Red Skull', 9);
+insert into option (option_value, poll_id) values ('Ego', 9);
+insert into option (option_value, poll_id) values ('Dormamu', 9);
 
 -- 10 Favorite Marvel villain
 insert into poll (poll_id, question) values (10, 'Which movie do you want to see?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'Mortal Kombat', 10);
-insert into option (option_id, option_value, poll_id) values (2, 'Black Widow', 10);
-insert into option (option_id, option_value, poll_id) values (3, 'Dune', 10);
+insert into option (option_value, poll_id) values ('Mortal Kombat', 10);
+insert into option (option_value, poll_id) values ('Black Widow', 10);
+insert into option (option_value, poll_id) values ('Dune', 10);
 
 -- 11 Favorite Red Rising
 insert into poll (poll_id, question) values (11, 'Which Red Rising book is the best?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'Red Rising', 11);
-insert into option (option_id, option_value, poll_id) values (2, 'Morning Star', 11);
-insert into option (option_id, option_value, poll_id) values (3, 'Golden Son', 11);
+insert into option (option_value, poll_id) values ('Red Rising', 11);
+insert into option (option_value, poll_id) values ('Morning Star', 11);
+insert into option (option_value, poll_id) values ('Golden Son', 11);
 
 -- 12 Favorite Video Game
 insert into poll (poll_id, question) values (12, 'Which video game series do you prefer?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'Fallout', 12);
-insert into option (option_id, option_value, poll_id) values (2, 'Doom', 12);
-insert into option (option_id, option_value, poll_id) values (3, 'Infamous', 12);
+insert into option (option_value, poll_id) values ('Fallout', 12);
+insert into option (option_value, poll_id) values ('Doom', 12);
+insert into option (option_value, poll_id) values ('Infamous', 12);
 
 -- 13 Favorite Director
 insert into poll (poll_id, question) values (13, 'Who is your favorite director?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'Christopher Nolan', 13);
-insert into option (option_id, option_value, poll_id) values (2, 'Quentin Tarantino', 13);
-insert into option (option_id, option_value, poll_id) values (3, 'Martin Scorcese', 13);
+insert into option (option_value, poll_id) values ('Christopher Nolan', 13);
+insert into option (option_value, poll_id) values ('Quentin Tarantino', 13);
+insert into option (option_value, poll_id) values ('Martin Scorcese', 13);
 
 -- 14 Favorite Film Genre
 insert into poll (poll_id, question) values (14, 'What is your favorite film genre?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'Horror/Thriller', 14);
-insert into option (option_id, option_value, poll_id) values (2, 'Comedy', 14);
-insert into option (option_id, option_value, poll_id) values (3, 'Romance', 14);
+insert into option (option_value, poll_id) values ('Horror/Thriller', 14);
+insert into option (option_value, poll_id) values ('Comedy', 14);
+insert into option (option_value, poll_id) values ('Romance', 14);
 
 -- 15 Favorite Art Style
 insert into poll (poll_id, question) values (15, 'What is your favorite art style?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'Impressionism', 15);
-insert into option (option_id, option_value, poll_id) values (2, 'Realism', 15);
-insert into option (option_id, option_value, poll_id) values (3, 'Abstract', 15);
+insert into option (option_value, poll_id) values ('Impressionism', 15);
+insert into option (option_value, poll_id) values ('Realism', 15);
+insert into option (option_value, poll_id) values ('Abstract', 15);
